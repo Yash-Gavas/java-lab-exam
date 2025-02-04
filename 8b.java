9e18ab76f0e4d3be987c6288f223298cb5a3f1cc
Yash Suresh Gavas <123618257+Yash-Gavas@users.noreply.github.com>
2025-02-04 20:34:40 +0530

    Update 8b.java

--- 8b.java
+++ 8b.java
@@ -1,4 +1,4 @@
-package a1;
+package a;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -6,65 +6,53 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import java.io.*;
+@WebServlet("/vote")
+public class vote extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        
+	        Cookie cookie1 = new Cookie("Cookie1", "Value1");
+	        Cookie cookie2 = new Cookie("Cookie2", "Value2");
+	        Cookie cookie3 = new Cookie("Cookie3", "Value3");
+	        Cookie cookie4 = new Cookie("Cookie4", "Value4");
+	        
+	        cookie1.setMaxAge(60); 
+	        cookie2.setMaxAge(60);
+	        
+	        response.addCookie(cookie1);
+	        response.addCookie(cookie2);
+	        response.addCookie(cookie3);
+	        response.addCookie(cookie4);
+	        
+	        Cookie[] cookies = request.getCookies();
+	
+	        out.println("<h2>Cookies Status</h2>");
+	
+	        if (cookies != null) 
+	        {
+	            for (Cookie cookie : cookies) 
+	            {
+	                out.println("<p>" + cookie.getName() + ": " + cookie.getValue() + "</p>");
+	            }
+	        } 
+	        
+	        else 
+	        {
+	            out.println("<p>No cookies found. Some cookies might have expired.</p>");
+	        }
+	
+	        out.println("<p>Refresh the page to see the effect of cookie expiration.</p>");
+	}
 
-@WebServlet("/first")
-public class first extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        // Retrieve cookies from the request
-        Cookie[] cookies = request.getCookies();
-
-        out.println("<html>");
-        out.println("<head><title>Cookie Display</title></head>");
-        out.println("<body>");
-        out.println("<h1>Cookie Display</h1>");
-
-        if (cookies != null && cookies.length > 0) {
-            out.println("<h3>All Available Cookies:</h3>");
-            out.println("<ul>");
-            for (Cookie cookie : cookies) {
-                out.println("<li>" + cookie.getName() + ": " + cookie.getValue() + "</li>");
-            }
-            out.println("</ul>");
-        } else {
-            out.println("<p>No cookies found. They may have expired.</p>");
-        }
-
-        out.println("<h3>Refresh the page to see which cookies remain.</h3>");
-        out.println("</body>");
-        out.println("</html>");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Create 4 cookies
-        Cookie cookie1 = new Cookie("shortLivedCookie1", "value1");
-        cookie1.setMaxAge(60); // 1 minute expiration
-        response.addCookie(cookie1);
-
-        Cookie cookie2 = new Cookie("shortLivedCookie2", "value2");
-        cookie2.setMaxAge(60); // 1 minute expiration
-        response.addCookie(cookie2);
-
-        Cookie cookie3 = new Cookie("longLivedCookie1", "value3");
-        cookie3.setMaxAge(3600); // 1 hour expiration
-        response.addCookie(cookie3);
-
-        Cookie cookie4 = new Cookie("longLivedCookie2", "value4");
-        cookie4.setMaxAge(3600); // 1 hour expiration
-        response.addCookie(cookie4);
-
-        // Redirect to the same servlet to display cookies
-        response.sendRedirect("first");
-    }
 }
-
-
 
 <!DOCTYPE html>
 <html lang="en">
